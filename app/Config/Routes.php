d510b767784740b4571a34cc5409d307058b3694
irchamali <id.irchamali@gmail.com>
2025-07-11 11:54:14 +0700

    Add MVC post and modify routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,4 +9,5 @@ $routes->get('/', 'Home::index');
 $routes->get('/about', 'Page::about');
 $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
-
+$routes->get('/post', 'Post::index');
+$routes->get('/post/(:any)', 'Post::viewPost/$1');
