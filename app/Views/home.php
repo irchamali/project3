2be37a8821080f14c5ab4d2a413fd0763481b435
irchamali <id.irchamali@gmail.com>
2025-07-24 14:36:02 +0700

    Add navbar for frontend and modify App

--- app/Views/home.php
+++ app/Views/home.php
@@ -12,33 +12,7 @@
 
 <body>
 
-	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-		<div class="container">
-			<a class="navbar-brand" href="<?= base_url() ?>">MyBlog</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="<?= base_url() ?>">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="<?= base_url('about') ?>">About</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="<?= base_url('post') ?>">Blog</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="<?= base_url('contact') ?>">Contact</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="<?= base_url('faqs') ?>">FAQ</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<?= $this->include('layouts/navbar'); ?>
 
 	<div class="p-5 mb-4 bg-light rounded-3">
       <div class="container py-5">
